24857c95639ce25f959b8373d69e601bbc220570
localTOAST <localtoast@eggandjam.com>
2013-11-29 17:58:07 -0800

    add iptables config

--- vagrant-templates/scripts/root_setup.sh
+++ vagrant-templates/scripts/root_setup.sh
@@ -20,3 +20,9 @@ sudo sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 sudo sed -i -e 's/Port 22/Port 18500/' /etc/ssh/sshd_config
 sudo sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sudo sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+# iptables
+sudo wget -q -N https://raw.github.com/juliangiuca/aws_automation/master/iptables.firewall.rules > /etc/iptables.firewall.rules -P /etc
+sudo bash -c 'printf "#!/bin/sh\n/sbin/iptables-restore < /etc/iptables.firewall.rules\n" > /etc/network/if-pre-up.d/firewall'
+sudo chmod +x /etc/network/if-pre-up.d/firewall
+
