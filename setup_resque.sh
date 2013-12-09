093f4777dc5b6d20181de7e2af5cc88f87ca8600
localTOAST <localtoast@eggandjam.com>
2013-12-08 21:57:42 -0800

    lower the worker count. Fix the node upstart script

--- setup_resque.sh
+++ setup_resque.sh
@@ -18,6 +18,15 @@ else
   APP_USER=$2;
 fi
 
+
+if [ -z "$3" ]
+then
+  echo "Please enter the user to run the service";
+  read APP_WORKER_COUNT;
+else
+  APP_WORKER_COUNT=$2;
+fi
+
 RESQUE_WORKER_FILE="/etc/init/$APP_NAME-resque-worker.conf"
 sudo sh -c "curl -s -L https://raw.github.com/juliangiuca/aws_automation/master/conf/resque-worker.conf \
   | sed \"s/APP_NAME/$APP_NAME/g\" \
@@ -28,4 +37,5 @@ RESQUE_WORKERS_FILE="/etc/init/$APP_NAME-resque-workers.conf"
 sudo sh -c "curl -s -L https://raw.github.com/juliangiuca/aws_automation/master/conf/resque-workers.conf \
   | sed \"s/APP_NAME/$APP_NAME/g\" \
   | sed \"s/APP_USER/$APP_USER/g\" \
+  | sed \"s/APP_WORKER_COUNT/$APP_WORKER_COUNT/g\" \
   > $RESQUE_WORKERS_FILE"
