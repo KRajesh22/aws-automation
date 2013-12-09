f1ca48372606df3ac53646bf640f4b9d361a5736
localTOAST <localtoast@eggandjam.com>
2013-12-08 21:13:37 -0800

    add resque upstart conf magic

---
+++ setup_resque.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]
+then
+  echo "Please enter an app name"
+  read APP_NAME;
+else
+  APP_NAME=$1;
+fi
+
+if [ -z "$2" ]
+then
+  echo "Please enter the user to run the service";
+  read APP_USER;
+else
+  APP_USER=$2;
+fi
+
+RESQUE_WORKER_FILE="/etc/init/$APP_NAME-resque-worker.conf"
+sudo sh -c "curl -s -L https://raw.github.com/juliangiuca/aws_automation/master/conf/resque-worker.conf \
+  | sed \"s/APP_NAME/$APP_NAME/g\" \
+  | sed \"s/APP_USER/$APP_USER/g\" \
+  > $RESQUE_WORKER_FILE"
+
+RESQUE_WORKERS_FILE="/etc/init/$APP_NAME-resque-workers.conf"
+sudo sh -c "curl -s -L https://raw.github.com/juliangiuca/aws_automation/master/conf/resque-workers.conf \
+  | sed \"s/APP_NAME/$APP_NAME/g\" \
+  | sed \"s/APP_USER/$APP_USER/g\" \
+  > $RESQUE_WORKERS_FILE"
