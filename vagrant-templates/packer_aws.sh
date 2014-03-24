2351382db5661a35dcf63df0ed96ba4df3076fe0
localTOAST <localtoast@eggandjam.com>
2014-03-23 17:33:59 -0700

    remove all the vagrant stuff - there's a better home for that. Simplify the upstart runner

--- vagrant-templates/packer_aws.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-packer build \
-  -var "aws_access_key=`cat .env | grep AWS_ID | sed 's/AWS_ID=//'`" \
-  -var "aws_secret_key=`cat .env | grep AWS_KEY | sed 's/AWS_KEY=//'`" \
-  -only=aws \
-  node_and_ruby_packer.json
